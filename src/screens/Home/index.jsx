1fdaa38a723b075a764e2c960c39f9eb647f29c2
AugusttoDaniel2001 <72755932+AugusttoDaniel2001@users.noreply.github.com>
2023-04-07 18:05:13 -0300

    4
    Formatação da tela home

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -3,22 +3,27 @@ import { View, Text, StyleSheet } from 'react-native';
 import CalendarPicker from 'react-native-calendar-picker';
 
 export default function Home() {
-  const [selectedDate, setSelectedDate] = useState(null);
+    const [selectedDate, setSelectedDate] = useState(null);
 
-  const onDateChange = (date) => {
-    setSelectedDate(date);
-  };
+    const onDateChange = (date) => {
+        setSelectedDate(date);
+    };
 
-  return (
-    <View>
-      <CalendarPicker  onDateChange={onDateChange} />
-    </View>
-  );
+    return (
+        <View style={[styles.container]}>
+            <View style={[styles.calendario]}>
+                <CalendarPicker onDateChange={onDateChange} />
+            </View>
+        </View>
+    );
 }
 const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#000',
-      alignItems: 'center',
+        flex: 1,
+        alignItems: 'center',
     },
-  }); 
+    calendario: {
+        marginTop: 100, 
+        marginHorizontal: 50,
+    }   
+}); 
