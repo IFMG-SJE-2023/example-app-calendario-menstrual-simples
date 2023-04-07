2c87df7dfdeeca6923ed629172551a107ab0ceb6
AugusttoDaniel2001 <72755932+AugusttoDaniel2001@users.noreply.github.com>
2023-04-07 17:52:41 -0300

    3
    Adicionando calendário na Home

--- src/screens/Home/index.jsx
+++ src/screens/Home/index.jsx
@@ -1,29 +1,24 @@
-import{View,Text, StyleSheet, TouchableOpacity} from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-export default function Home(){
-    const navigation = useNavigation();
-return(
-    <View style={styles.container}>
-        <Text style={styles.title}>Home</Text>
-        <TouchableOpacity on onPress={()=> navigation.goBack()}>
-            <Text>Voltar</Text>
-        </TouchableOpacity>
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import CalendarPicker from 'react-native-calendar-picker';
+
+export default function Home() {
+  const [selectedDate, setSelectedDate] = useState(null);
+
+  const onDateChange = (date) => {
+    setSelectedDate(date);
+  };
+
+  return (
+    <View>
+      <CalendarPicker  onDateChange={onDateChange} />
     </View>
-)
-
+  );
 }
-
-const styles = StyleSheet.create(
-    {
-        container:{
-            flex:1,
-            alignItems:'center',
-            justifyContent:'center',
-        },
-        title:{
-            color:'orange',
-            fontSize:48,
-            fontWeight:'bold',
-        },
-    }
-)
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#000',
+      alignItems: 'center',
+    },
+  }); 
