f32b324813f45ae61bb66c5902247056113f69a7
AugusttoDaniel2001 <72755932+AugusttoDaniel2001@users.noreply.github.com>
2023-04-09 16:29:37 -0300

    9
    Eu n lembro oq eu fiz aq mas to mandando pra branch

--- src/components/Tabnavigator.js
+++ src/components/Tabnavigator.js
@@ -3,15 +3,40 @@ import { createStackNavigator } from '@react-navigation/stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Login from '../screens/Login';
 import Home from '../screens/Home';
-
+import Dicas from '../screens/Dicas';
+import Cadastro from '../screens/Cadastro';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
 
+const TabBarIcon = ({ name, color, size }) => {
+  return <Ionicons name={name} color={color} size={size} />;
+};
+
 const TabNavigator = () => {
   return (
     <Tab.Navigator>
-      <Tab.Screen name="tabHome" component={Home} options={{ headerShown: false }} />
+      <Tab.Screen
+        name="Home"
+        component={Home}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <TabBarIcon name="ios-home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tab.Screen
+        name="Dicas"
+        component={Dicas}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color, size }) => (
+            <TabBarIcon name="ios-home" color={color} size={size} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 };
@@ -20,6 +45,7 @@ const AppNavigator = () => {
   return (
     <Stack.Navigator initialRouteName="Login">
       <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+      <Stack.Screen name="Cadastro" component={Cadastro} options={{ headerShown: false }} />
       <Stack.Screen name="Home" component={TabNavigator} />
     </Stack.Navigator>
   );
---
+++ src/screens/Dicas/index.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+
+export default function Dicas() {
+    
+    return (
+        <View style={[styles.container]}>
+
+        </View>
+    );
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+
+    },
+}); 
